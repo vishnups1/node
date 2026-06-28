61f2de95eb1c4d594b02edaf676a541c9f41e376
v01 <v01@yahooinc.com>
2026-06-28 21:44:30 +0530

    feat: added README

--- node-js-cc/packages-demo/app.js
+++ node-js-cc/packages-demo/app.js
@@ -3,12 +3,22 @@
 
 import {PI} from "./pi/index.js"; // Explicitly name the file
 import express from "express"; // npm install express
+import axios  from "axios";
 
 const app = express()
 const PORT = 8080
 
 app.get("/pi", (req, res) => {
     res.send(PI);
+})
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get('https://httpbin.org/json');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
 });
 
 app.listen(PORT, () => {
